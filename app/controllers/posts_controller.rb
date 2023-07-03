5f6a6104fa4d4216e74d43a7c3a31e7efc4c75c7
iwasaki tomoya <tomoya02201998@gmail.com>
2023-07-03 09:47:48 +0000

    2-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,14 @@ class PostsController < ApplicationController
   def new
     render :new
   end
+  
+  def index
+    render :index
+  end
+  
+  def create
+    redirect_to 'posts/index'
+  end
+  
 end
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
- get 'posts/new', to: 'posts#new', as: 'new_post'
-
-#TopicsController
- get 'posts/new', to: 'Topics#new', as: 'new_post'
+ 
+# PostsController
+ get'posts/new',to:'posts#new',as:'new_posts'
+ get 'posts/index', to: 'posts#index', as: 'index_Action'
  
 #TopicsController
- get 'posts/new', to: 'Topics#new', as: 'edit_post'
+ get'topics/new',to:'topics#new',as:'new_Action'
+ 
+
+
  end
