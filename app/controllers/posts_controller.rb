8ea342714e1b9bcefb110bcbbc6dacfd54a031dd
iwasaki tomoya <tomoya02201998@gmail.com>
2023-07-09 08:11:03 +0000

    2-10

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,19 +2,34 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
  
   def index
-    render 'posts/index'
+    @title = params[:title]
+    if @title.present?
+      @posts = Post.where('title LIKE ?', "%#{@title}%")
+    else
+      @posts = Post.all
+    end
+    render :index
+  
   end
   
-  
   def new
-    render :new # renders app/views/posts/new.html.erb
+    @posts = Post.new
+    render :new
   end
 
   def create
-    redirect_to new_post_path # redirects to GET "/posts/new"
-  end
+    @posts = Post.new(post_params)
 
-  
+    if params[:post][:image]
+      @posts.image.attach(params[:post][:image])
+    end
+
+    if @posts.save
+      redirect_to index_post_path, notice: '登録しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
   def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,2 @@
 class Post < ApplicationRecord
-  # Validation
-  validates :title, presence: true
-  validates :body, presence: true
-
-  # ActiveStorage
-　has_one_attached :image
 end
