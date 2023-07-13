fe875b5b641dff0c3986e97013209198107873d3
iwasaki tomoya <tomoya02201998@gmail.com>
2023-07-13 13:52:00 +0000

    2-10

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,28 +6,19 @@ class TopicsController < ApplicationController
 
   def create
     @topic = Topic.new(topic_params)
-
-    if params[:topic][:image]
-      @topic.image.attach(params[:topic][:image])
-    end
-
-    if @topic.save
-      redirect_to index_topic_path, notice: '登録しました'
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
 
-  private
-  def topic_params
-    params.require(:topic).permit(:title, :body, :image)
-  end
+end
 
-  def edit
-    render :edit
-  end
+private
+def topic_params
+  params.require(:topic).permit(:title)
+end
 
-  def update
-    redirect_to edit_topics_path
-  end
-end 
+def edit
+  render :edit
+end
+
+def update
+  redirect_to edit_topics_path
+end
